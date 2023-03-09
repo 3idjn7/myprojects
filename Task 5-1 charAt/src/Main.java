88f311d694446831c85db35d03dfc2a67bb012c7
3idjn7 <seprefy@gmail.com>
2023-03-09 16:53:05 +0200

    Test

--- Task 5-1 charAt/src/Main.java
+++ Task 5-1 charAt/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+        String text = "An \"example sentence\" is a sentence written to demonstrate usage of a particular word in context. An example sentence is invented by its writer to show how to use a particular word properly in writing. Such examples are placed following a given definition, to make it clear which definition they illustrate.";
         int count = findA(text);
         System.out.println("The count of 'a' in the text is: " + count);
     }
